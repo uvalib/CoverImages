4c25f31f7ac61075f1c3a231ffff2e561215ac7f
Nestor Walker <nestor@walker.io>
2016-09-29 14:51:40 -0400

    production sidekiq config

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -3,6 +3,16 @@ Sidekiq.configure_server do |config|
   # production redis config goes here
   # https://github.com/mperham/sidekiq/wiki/Advanced-Options
 
+  if ENV["REDIS_URL"].present?
+    Sidekiq.configure_server do |config|
+      config.redis = { url: ENV["REDIS_URL"], namespace: "coverImages", password: ENV["REDIS_PASSWORD"] }
+    end
+
+    Sidekiq.configure_client do |config|
+      config.redis = { url: ENV["REDIS_URL"], namespace: "coverImages", password: ENV["REDIS_PASSWORD"] }
+    end
+  end
+
   config.server_middleware do |chain|
     chain.add Sidekiq::Throttler
   end
