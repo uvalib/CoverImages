0184a41f5f1ad7cb206871d3ce070ae66d23ea07
Nestor Walker <nestor@walker.io>
2016-10-10 13:30:44 -0400

    proper puma config location

--- config/deploy.rb
+++ config/deploy.rb
@@ -32,7 +32,7 @@ set :keep_assets, 5
 # set :pty, true
 
 # Default value for :linked_files is []
-append :linked_files, 'config/database.yml', 'config/secrets.yml', '.env.production', 'puma.rb'
+append :linked_files, 'config/database.yml', 'config/secrets.yml', '.env.production', 'config/puma.rb'
 
 # Default value for linked_dirs is []
 append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system'
