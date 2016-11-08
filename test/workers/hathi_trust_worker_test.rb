82ce83eac8b0d673e867a3147fe41959844e9718
Nestor Walker <naw4t@virginia.edu>
2016-11-08 16:46:00 -0500

    fix Hathi Trust VCR test

--- test/workers/hathi_trust_worker_test.rb
+++ test/workers/hathi_trust_worker_test.rb
@@ -3,7 +3,15 @@ class HathiTrustWorkerTest < ActiveSupport::TestCase
 
   def test_good_cover_image
 
-    VCR.use_cassette('known_good_hathi_trust') do
+    VCR.use_cassette(
+      'known_good_hathi_trust',
+      match_requests_on: [
+        :method, :path,
+        VCR.request_matchers.uri_without_params(
+          :oauth_timestamp, :oauth_nonce, :oauth_signature
+        )
+      ]
+    ) do
       Sidekiq::Testing.inline!
       @cover_image = create(:known_hathi_trust)
       HathiTrustWorker.perform_async(@cover_image.id)
