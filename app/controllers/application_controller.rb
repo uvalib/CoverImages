ef35c1df4775139772a1947fd7018aae143c9ffc
Nestor Walker <naw4t@virginia.edu>
2025-03-27 11:50:24 -0400

    use rails remote_ip

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,10 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(_resource)
     root_path
   end
+
+  private
+  def append_info_to_payload(payload)
+    super
+    payload.merge!(remote_ip: request.remote_ip)
+  end
 end
--- config/application.rb
+++ config/application.rb
@@ -33,7 +33,7 @@ module CoverImages
 
     config.lograge.enabled = true
     config.lograge.custom_options = lambda do |event|
-      { remote_ip: event.payload[:headers]["REMOTE_ADDR"] }
+      { remote_ip: event.payload[:remote_ip] }
     end
     class Lograge::Formatters::LogFormatter < Lograge::Formatters::KeyValue
       def format(key, value)
